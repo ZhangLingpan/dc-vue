44575bfbc6aa728e78a8424d8aa3754c1a03ff6c
zhangliangpan <zhangliangpan@supremind.com>
2022-10-28 15:15:52 +0800

    add layer

--- src/api/ViewerApi.js
+++ src/api/ViewerApi.js
@@ -3,7 +3,7 @@
  * @Date: 2020-05-07 19:28:33
  */
 
-class ViewerApi {
+ class ViewerApi {
   constructor(viewer) {
     this._viewer = viewer
   }
@@ -13,10 +13,19 @@ class ViewerApi {
   }
 
   addBaseLayer() {
-    let baidu = DC.ImageryLayerFactory.createAmapImageryLayer({
-      style: 'img'
+    let xyz = DC.ImageryLayerFactory.createXYZImageryLayer({
+      url: 'http://100.100.151.221:1800/mapcache/gmaps/smgis_sd_dark@g_hd/{z}/{x}/{y}.png'
     })
-    this._viewer.addBaseLayer(baidu)
+    this._viewer.addBaseLayer(xyz)
+    this._viewer.compass.enable = true
+    let layer = new DC.TilesetLayer('layer')
+    this._viewer.addLayer(layer)
+    let tileset = new DC.Tileset(
+      'http://100.100.151.221:1800/mapcache/offline/building/tileset.json'
+    )
+    //tileset.setHeight(0)
+    layer.addOverlay(tileset)
+    this._viewer.flyTo(tileset)
     return this
   }
 }
