bcd090e8d0601132595793bf0cfe8a72345017a5
zhangliangpan <zhangliangpan@supremind.com>
2022-10-31 10:01:03 +0800

    update

--- src/api/ViewerApi.js
+++ src/api/ViewerApi.js
@@ -14,15 +14,27 @@ class ViewerApi {
 
   addBaseLayer() {
     let xyz = DC.ImageryLayerFactory.createXYZImageryLayer({
-      url: 'http://100.100.151.221:1800/mapcache/gmaps/smgis_sd_dark@g_hd/{z}/{x}/{y}.png'
+      url: 'http://100.100.142.135:1800/mapcache/gmaps/smgis_sd_dark@g_hd/{z}/{x}/{y}.png'
     })
-    this._viewer.addBaseLayer(xyz)
+    let hdlayer = new DC.ImageryLayerFactory.createXYZImageryLayer({
+      url:'https://mapinner-test.wz-inc.com/mapcache/gmaps/futurecity_sxsy_mars_dark@g_hd/{z}/{x}/{y}.png'
+    })
+    this._viewer.addBaseLayer([xyz,hdlayer])
     this._viewer.compass.enable = true
+    this.viewer.distanceLegend.enable = true
+    this.viewer.locationBar.enable = true
     let layer = new DC.TilesetLayer('layer')
     this._viewer.addLayer(layer)
     let tileset = new DC.Tileset(
-      'http://100.100.151.221:1800/mapcache/offline/building/tileset.json'
+      'http://100.100.142.135:1800/mapcache/offline/building/tileset.json'
     )
+    let style = new DC.TilesetStyle()
+    style.color = {
+    conditions: [
+      ['true', 'rgb(32, 24, 255)'],
+    ],
+    }
+    tileset.setStyle(style)
     //tileset.setHeight(0)
     layer.addOverlay(tileset)
     this._viewer.flyTo(tileset)
